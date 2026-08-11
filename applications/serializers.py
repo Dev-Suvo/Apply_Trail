098daf47f687fdecda514195f3e22669a6d41598
Dev-Suvo <subodipdey20@gmail.com>
2026-08-11 20:48:13 +0530

    nested serializer(POST & ALL except GET)

--- applications/serializers.py
+++ applications/serializers.py
@@ -10,10 +10,14 @@ class TagSerializer(serializers.ModelSerializer):
 
 
 class Applicationserializers(serializers.ModelSerializer):
-    tags =TagSerializer(many = 'True')
+    tags = TagSerializer(many=True,read_only=True)
+    
+    tag_ids =serializers.PrimaryKeyRelatedField(many =True,source='tags',write_only=True, queryset=Tag.objects.all())
     class Meta:
         model = Application
         fields = ['id', 'user', 'company', 'role', 'status',
             'applied_date', 'notes',
-            'created_at', 'updated_at', 'tags']
+            'created_at', 'updated_at', 'tags','tag_ids']
         read_only_fields = [ 'created_at', 'updated_at']
+
+
